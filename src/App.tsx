079e12448887d413220b5fa9b06e362e7b79aaab
Michal Gawik <gawlik.zator@o2.pl>
2024-09-25 09:34:28 +0200

    add logic to buttons

--- src/App.tsx
+++ src/App.tsx
@@ -9,6 +9,37 @@ function App() {
 
 	const { name, job, image, text } = reviews[person]
 
+	const checkNumber = (number: number): number => {
+		if (number > reviews.length - 1) {
+			return 0
+		}
+		if (number < 0) {
+			return reviews.length - 1
+		}
+		return number
+	}
+
+	const nextPerson = () => {
+		setPerson(person => {
+			let newIndex = person + 1
+			return checkNumber(newIndex)
+		})
+	}
+	const prevPerson = () => {
+		setPerson(person => {
+			let newIndex = person - 1
+			return checkNumber(newIndex)
+		})
+	}
+
+	const randomUser = () => {
+		let randomNumber = Math.floor(Math.random() * reviews.length)
+		if (randomNumber === person) {
+			randomNumber = person + 1
+		}
+		setPerson(checkNumber(randomNumber))
+	}
+
 	return (
 		<main>
 			<article className='review'>
@@ -22,20 +53,14 @@ function App() {
 				<p className='job'>{job}</p>
 				<p className='info'>{text}</p>
 				<div className='btn-container'>
-					<button
-						className='prev-btn'
-						type='button'
-						onClick={() => setPerson(person - 1)}>
+					<button className='prev-btn' type='button' onClick={prevPerson}>
 						<GrPrevious />
 					</button>
-					<button
-						className='next-btn'
-						type='button'
-						onClick={() => setPerson(person + 1)}>
+					<button className='next-btn' type='button' onClick={nextPerson}>
 						<GrNext />
 					</button>
 				</div>
-				<button className='btn btn-hipster' onClick={() => {}}>
+				<button className='btn btn-hipster' onClick={() => randomUser()}>
 					random user
 				</button>
 			</article>
